f8209fa114285b3c992521ae86ad2555b9f43cdc
Philippe Luchansky <philuchansky@gmail.com>
2018-07-03 10:35:48 -0700

    mongoose connection

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ require('dotenv').config()
 const
   express = require('express'),
   app = express(),
-  { PORT } = process.env
+  mongoose = require('mongoose'),
+  { PORT, MONGODB_URI } = process.env
+
+mongoose.connect(MONGODB_URI, (err) => {
+  console.log(err || "Connected to MongoDB")
+})
 
 app.get('/', (req, res) => {
   res.json({ message: "root" })
