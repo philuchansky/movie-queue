e2c15dda65e7b553fa27c704f0d350a4b5769521
Philippe Luchansky <philuchansky@gmail.com>
2018-07-03 13:03:20 -0700

    dispatch getFeaturedMoviesSuccess when data is returned

--- client/src/components/FeaturedMovies/index.jsx
+++ client/src/components/FeaturedMovies/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { getFeaturedMovies } from '../../actions/movies'
 
-export default (props) => (
-  <h1>Featured Movies Here</h1>
-)
+class FeaturedMovies extends React.Component {
+  componentDidMount() {
+    this.props.getFeaturedMovies()
+  }
+
+  render() {
+    return (
+      <h1>Featured Movies Here</h1>
+    )
+  }
+}
+
+const mapStateToProps = ({ featuredMovies }) => ({ featuredMovies })
+export default connect(mapStateToProps, { getFeaturedMovies })(FeaturedMovies)
--- client/src/reducers/featuredMovies.js
+++ client/src/reducers/featuredMovies.js
@@ -1,5 +1,9 @@
+import { GET_FEATURED_MOVIES } from '../types/movies'
+
 export default (state = [], action) => {
   switch(action.type) {
+    case GET_FEATURED_MOVIES.SUCCESS:
+      return action.payload
     default: return state
   }
 }
