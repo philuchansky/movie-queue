350027b0618985aa629ba8f128f572c5de10bdbf
Philippe Luchansky <philuchansky@gmail.com>
2018-07-03 12:40:17 -0700

    set up redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
-    return (
+    return ( 
       <div className="App">
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
+import thunk from 'redux-thunk'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
