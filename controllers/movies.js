402c087cafdb9ae2e56536e3c97d9c08b7c1c167
Philippe Luchansky <philuchansky@gmail.com>
2018-07-03 12:12:31 -0700

    use cheerio to get new featured movie list

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,5 +1,22 @@
+const
+  { OMDB_API_KEY } = process.env,
+  cheerio = require('cheerio'),
+  axios = require('axios'),
+  apiClient = axios.create()
+
 module.exports = {
   index: (req, res) => {
-    res.json({ message: 'movies root.' })
+    apiClient ({ method: 'get', url: 'https://www.fandango.com' }).then(({ data }) => {
+      const $ = cheerio.load(data)
+      const featuredMovies = []
+      $('#homeMovieCarousel li').each(function (i, el) {
+        const movie = {
+          title: $(this).text().trim().split('\n').shift().replace(/ *\([^)]*\) */g, ""),
+          img: $(this).find('img').attr('src').replace("//", "")
+        }
+        featuredMovies.push(movie)
+      })
+      res.json(featuredMovies)
+    })
   }
 }
