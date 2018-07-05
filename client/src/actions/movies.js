69929c96b10cbf62449881efbaf21e5c4fe98f91
Philippe Luchansky <philuchansky@gmail.com>
2018-07-05 09:14:36 -0700

    getMovie action dispatcher

--- client/src/actions/movies.js
+++ client/src/actions/movies.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { GET_FEATURED_MOVIES } from '../types/movies'
+import { GET_FEATURED_MOVIES, GET_MOVIE } from '../types/movies'
 
 const getFeaturedMoviesLoading = () => ({ type: GET_FEATURED_MOVIES.LOADING })
 // const getFeaturedMoviesError = () => ({ type: GET_FEATURED_MOVIES.ERROR })
@@ -9,6 +9,18 @@ export function getFeaturedMovies() {
     dispatch(getFeaturedMoviesLoading())
     axios('/api/movies').then(({ data }) => {
       dispatch(getFeaturedMoviesSuccess(data))
+    })
+  }
+}
+
+const getMovieLoading = () => ({ type: GET_MOVIE.LOADING })
+// const getMovieError = () => ({ type: GET_MOVIE.ERROR })
+const getMovieSuccess = (movie) => ({ type: GET_MOVIE.SUCCESS, payload: movie })
+export function getMovie(title) {
+  return (dispatch) => {
+    dispatch(getMovieLoading())
+    axios(`/api/movies/${title}`).then(({ data }) => {
+      dispatch(getMovieSuccess(data))
     })
   }
 }
