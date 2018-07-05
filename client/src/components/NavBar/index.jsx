14ccf67ff3d5f7215f6ff78663e47ca4746c51ac
Philippe Luchansky <philuchansky@gmail.com>
2018-07-05 10:32:10 -0700

    <NavBar /> structure

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -5,8 +5,17 @@ export default (props) => (
   <nav className="navbar" aria-label="main navigation">
     <div className="navbar-brand">
       <Link className="navbar-item" to="/">Movie Queue</Link>
+      <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
     </div>
     <div className="navbar-menu">
+      <div class="navbar-start">
+        <Link to="/" className="navbar-item">Home</Link>
+        <Link to="/queue" className="navbar-item">My Queue</Link>
+      </div>
     </div>
   </nav>
 )
