4dd9634c613919d8fad1f510fb7e5140ab9df9bf
Philippe Luchansky <philuchansky@gmail.com>
2018-07-05 09:00:33 -0700

    stubbed <MovieDetail />

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import FeaturedMovies from './components/FeaturedMovies'
+import MovieDetail from './components/MovieDetail'
 
 class App extends Component {
   render() {
@@ -10,9 +11,7 @@ class App extends Component {
           <Route path="/test" render={() => (
             <h1>test</h1>
           )} />
-          <Route path="/movies/:title" render={() => (
-            <h1>Movie Details</h1>
-          )} />
+          <Route path="/movies/:title" component={MovieDetail} />
           <Route path="/" component={FeaturedMovies} />
         </Switch>
       </div>
---
+++ client/src/components/MovieDetail/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MovieDetail extends React.Component {
+  render() {
+    return (
+      <div className="MovieDetail">
+        <h1>Movie Detail</h1>
+      </div>
+    )
+  }
+}
+
+export default MovieDetail
