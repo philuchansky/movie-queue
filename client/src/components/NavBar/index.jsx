d93c69dabe4580936dfef143f713fb3aee0c583c
Philippe Luchansky <philuchansky@gmail.com>
2018-07-05 11:22:59 -0700

    toggle mobile nav

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,21 +1,35 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default (props) => (
-  <nav className="navbar" aria-label="main navigation">
-    <div className="navbar-brand">
-      <Link className="navbar-item" to="/">Movie Queue</Link>
-      <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-    <div className="navbar-menu">
-      <div class="navbar-start">
-        <Link to="/" className="navbar-item">Home</Link>
-        <Link to="/queue" className="navbar-item">My Queue</Link>
-      </div>
-    </div>
-  </nav>
-)
+class NavBar extends React.Component {
+
+  state = { active: false }
+
+  toggleActive() {
+    this.setState({ active: !this.state.active })
+  }
+
+  render() {
+    const { active } = this.state
+    return (
+      <nav className="navbar" aria-label="main navigation">
+        <div className="navbar-brand">
+          <Link className="navbar-item" to="/">Movie Queue</Link>
+          <a onClick={this.toggleActive.bind(this)} role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div className={`navbar-menu ${active ? 'is-active' : '' }`}>
+          <div className="navbar-start">
+            <Link to="/" className="navbar-item">Home</Link>
+            <Link to="/queue" className="navbar-item">My Queue</Link>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default NavBar
