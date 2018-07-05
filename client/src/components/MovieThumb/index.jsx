eef1524d27c65381955fee880eebf53becde83a0
Philippe Luchansky <philuchansky@gmail.com>
2018-07-05 11:28:15 -0700

    use bulma card ui for <MovieThumb />

--- client/src/components/MovieThumb/index.jsx
+++ client/src/components/MovieThumb/index.jsx
@@ -4,11 +4,21 @@ import { Link } from 'react-router-dom'
 const MovieThumb = (props) => {
   const { movie } = props
   return (
-    <div className="MovieThumb">
-      <Link to={`/movies/${movie.title}`}>
-        <img src={movie.img} alt={movie.title} />
-        {movie.title}
-      </Link>
+    <div className="MovieThumb card">
+      <div className="card-image">
+        <figure className="image">
+          <Link to={`/movies/${movie.title}`}>
+            <img src={movie.img} alt={movie.title} />
+          </Link>
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="content">
+          <p className="title is-4">
+            <Link to={`/movies/${movie.title}`}>{movie.title}</Link>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
