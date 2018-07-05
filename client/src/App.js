a3e664db1d77f6ed3ff9f1bb014e97b098eebf6b
Philippe Luchansky <philuchansky@gmail.com>
2018-07-05 10:14:20 -0700

    fontawesome installed with example usage in <App />

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import FeaturedMovies from './components/FeaturedMovies'
 import MovieDetail from './components/MovieDetail'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 class App extends Component {
   render() {
     return ( 
       <div className="App">
+        {/* <FontAwesomeIcon icon={faCoffee} /> */}
         <Switch>
           <Route path="/test" render={() => (
             <h1>test</h1>
