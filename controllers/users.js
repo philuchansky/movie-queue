1fbd12a6fd3a6930a1ee06e3824fc8e2a6749211
Philippe Luchansky <philuchansky@gmail.com>
2018-07-05 22:39:47 -0700

    protected users#me routes (get, patch)

--- controllers/users.js
+++ controllers/users.js
@@ -4,15 +4,29 @@ const
 
 module.exports = {
   index: (req, res) => {
-    User.find({}).exec().then(users => res.json(users))
+    User.find({}).exec().then(users => {
+      res.json(users)
+    })
   },
 
   create: (req, res) => {
-    User.create(req.body).then(user => res.json({
-      success: true,
-      message: "user created",
-      user
-    }))
+    User.create(req.body).then(user => {
+      const token = generateToken(user)
+      res.json({ success: true, message: "user created, token attached", user, token })
+    })
+  },
+
+  me: (req, res) => {
+    res.json(req.user)
+  },
+
+  update: (req, res) => {
+    if(!req.body.password) delete req.body.password
+    Object.assign(req.user, req.body)
+    req.user.save().then(updatedUser => {
+      const token = generateToken(updatedUser)
+      res.json({ success: true, message: "user updated", token })
+    })
   },
 
   authenticate: (req, res) => {
@@ -21,7 +35,7 @@ module.exports = {
         res.json({ success: false, message: "invalid credentials" })
       } else {
         const token = generateToken(user)
-        res.json({ success: true, token })
+        res.json({ success: true, message: "token attached", token })
       }
     })
   }
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,19 @@
 const
   express = require('express'),
   usersRouter = new express.Router(),
-  usersCtrl = require('../controllers/users.js')
+  usersCtrl = require('../controllers/users.js'),
+  { verifyToken } = require('../auth.js')
 
 usersRouter.route('/')
   .get(usersCtrl.index)
   .post(usersCtrl.create)
 
 usersRouter.post('/authenticate', usersCtrl.authenticate)
+
+usersRouter.use(verifyToken)
+
+usersRouter.route('/me')
+  .get(usersCtrl.me)
+  .patch(usersCtrl.update)
 
 module.exports = usersRouter
