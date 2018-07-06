0bb23e1222f9d555b3c220446edc0aeeaafa7899
Philippe Luchansky <philuchansky@gmail.com>
2018-07-06 16:06:45 -0700

    styled <LogIn />

--- client/src/components/LogIn/Form.jsx
+++ client/src/components/LogIn/Form.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons'
 
 const Form = (props) => {
   const { handleSubmit } = props
   return (
     <form onSubmit={handleSubmit}>
-      <Field component="input" type="text" name="email" placeholder="Email" />
-      <Field component="input" type="password" name="password" placeholder="Password" />
-      <button>Log In</button>
+      <div className="field">
+        <label className="label is-medium">Email</label>
+        <div className="control has-icons-left has-icons-right">
+          <Field className="input is-medium" component="input" type="email" name="email" placeholder="Email" />
+          <span className="icon is-medium is-left">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </span>
+        </div>
+      </div>
+      <div className="field">
+        <label className="label is-medium">Password</label>
+        <div className="control has-icons-left has-icons-right">
+          <Field className="input is-medium" component="input" type="password" name="password" placeholder="Password" />
+          <span className="icon is-medium is-left">
+            <i className="fas fa-envelope fa-xs"></i>
+            <FontAwesomeIcon icon={faLock} />
+          </span>
+        </div>
+      </div>
+      <div className="field">
+        <div className="control">
+          <button className="button is-medium is-info">Log In</button>
+        </div>
+      </div>
     </form>
   )
 }
--- client/src/components/LogIn/index.jsx
+++ client/src/components/LogIn/index.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { logIn } from '../../actions/users'
 import Form from './Form'
 
 
 class LogIn extends React.Component {
+
+  onFormSubmit(credentials) {
+    this.props.logIn(credentials)
+  }
+
   render() {
     return (
       <div className="LogIn">
-        <h1>Log In</h1>
-        <Form onSubmit={(fields) => console.log(fields)} />
+        <h1 className="title">Log In</h1>
+        <Form onSubmit={this.onFormSubmit.bind(this)} />
       </div>
     )
   }
 }
 
-export default LogIn
+export default connect(null, { logIn })(LogIn)
