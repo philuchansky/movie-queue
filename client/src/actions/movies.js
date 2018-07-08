e8c459626f2cc6b03d96855e4039f202b725b7b3
Philippe Luchansky <philuchansky@gmail.com>
2018-07-08 12:18:25 -0700

    searchMovies action dispatcher

--- client/src/actions/movies.js
+++ client/src/actions/movies.js
@@ -1,5 +1,5 @@
 import httpClient from '../httpClient'
-import { GET_FEATURED_MOVIES, GET_MOVIE } from '../types/movies'
+import { GET_FEATURED_MOVIES, GET_MOVIE, SEARCH_MOVIES } from '../types/movies'
 
 const getFeaturedMoviesLoading = () => ({ type: GET_FEATURED_MOVIES.LOADING })
 // const getFeaturedMoviesError = () => ({ type: GET_FEATURED_MOVIES.ERROR })
@@ -21,6 +21,18 @@ export function getMovie(title) {
     dispatch(getMovieLoading())
     httpClient({ method: 'get', url: `/movies/${title}` }).then(({ data }) => {
       dispatch(getMovieSuccess(data))
+    })
+  }
+}
+
+const searchMoviesLoading = () => ({ type: SEARCH_MOVIES.LOADING })
+// const searchMoviesError = () => ({ type: SEARCH_MOVIES.ERROR })
+// const searchMoviesSuccess = (results) => ({ type: SEARCH_MOVIES.SUCCESS, payload: results })
+export function searchMovies(term) {
+  return (dispatch) => {
+    dispatch(searchMoviesLoading())
+    httpClient({ method: 'get', url: `/movies/search?q=${term}` }).then(({ data }) => {
+      console.log(data)
     })
   }
 }
