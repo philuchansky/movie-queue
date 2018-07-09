6ecaa4d1013ecc65fe68456d9caf3971e468b48b
Philippe Luchansky <philuchansky@gmail.com>
2018-07-08 22:33:27 -0700

    display formatted <Results /> in <SearchBar />

--- client/src/components/SearchBar/Results.jsx
+++ client/src/components/SearchBar/Results.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 const Results = (props) => {
   const { items } = props
-  console.log(items)
   return (
-    <nav className="panel">
+    <nav className="Results panel">
       {items.slice(0, 5).map(item => (
         <Link key={item.imdbID} to={`/movies/${item.Title}`} className="panel-block">
           <span className="panel-icon">
-            <i className="fas fa-book" aria-hidden="true"></i>
+            {/* <i className="fas fa-book" aria-hidden="true"></i> */}
+            <img src={item.Poster} alt={item.Title} />
           </span>
           {item.Title}
         </Link>
