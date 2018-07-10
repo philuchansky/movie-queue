435d5425b20cc858470bb3fb6f9b3210182f101f
Philippe Luchansky <philuchansky@gmail.com>
2018-07-10 10:19:57 -0700

    check for queue item existence before adding

--- controllers/queue.js
+++ controllers/queue.js
@@ -1,8 +1,13 @@
-const
-  User = require('../models/User.js')
-
 module.exports = {
   add: (req, res) => {
-    res.json(req.user)
+    const { user } = req
+    if(!user.queue.find((qi) => qi.TMDB_id === req.body.TMDB_id)) {
+      user.queue.push(req.body)
+      user.save().then(user => {
+        res.json({ success: true, message: "queue item added", queue: user.queue })
+      })
+    } else {
+      res.json({ success: false, message: "already in queue" })
+    }
   }
 }
