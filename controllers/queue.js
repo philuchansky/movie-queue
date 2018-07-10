1605b9331d83bcdf962a8994a8b04924a39e743d
Philippe Luchansky <philuchansky@gmail.com>
2018-07-10 12:38:05 -0700

    referenced QueueItem queue#destroy

--- controllers/queue.js
+++ controllers/queue.js
@@ -2,13 +2,13 @@ const QueueItem = require('../models/QueueItem.js')
 
 module.exports = {
   index: (req, res) => {
-    QueueItem.find({ user: req.user }).exec()
+    QueueItem.find({ user: req.user }).select('-user').exec()
       .then(queueItems => res.json(queueItems))
   },
 
   create: (req, res) => {
     QueueItem.create({ user: req.user, ...req.body })
-      .then(queueItem => res.json({ success: true, message: 'queue item added', queueItem }))
+      .then(queueItem => res.json({ success: true, message: 'queue item added.', queueItem }))
       .catch(err => {
         if(err.code === 11000) res.json({ success: false, message: 'queue item already exists.' })
         else res.json({ success: false, message: 'there was a problem.' })
@@ -16,6 +16,10 @@ module.exports = {
   },
 
   destroy: (req, res) => {
-    res.json({ message: 'deleting queue item' })
+    QueueItem.findByIdAndRemove(req.params.id).select('-user').exec()
+      .then(queueItem => {
+        if(!queueItem) return res.json({ success: false, message: 'no queue item to remove.' })
+        res.json({ success: true, message: 'queue item removed.', queueItem })
+      })
   }
 }
--- routes/queue.js
+++ routes/queue.js
@@ -10,6 +10,6 @@ queueRouter.route('/')
   .get(queueCtrl.index)
   .post(queueCtrl.create)
 
-queueRouter.delete('/:TMDB_id', queueCtrl.destroy)
+queueRouter.delete('/:id', queueCtrl.destroy)
 
 module.exports = queueRouter
