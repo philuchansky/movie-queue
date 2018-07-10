1ca51236846b6d29c5b9311bf479d59527929f10
Philippe Luchansky <philuchansky@gmail.com>
2018-07-09 19:34:20 -0700

    external link formatting

--- client/src/components/MovieDetail/index.jsx
+++ client/src/components/MovieDetail/index.jsx
@@ -4,6 +4,7 @@ import { getMovie } from '../../actions/movies'
 import { posterUrl, formattedDate } from '../../helpers'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faImdb } from '@fortawesome/free-brands-svg-icons'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 import MovieMeta from './MovieMeta'
 import Score from '../Score'
 import './MovieDetail.css'
@@ -34,11 +35,20 @@ class MovieDetail extends React.Component {
               <MovieMeta label="Release Date" value={formattedDate(movie.release_date)} />
               <MovieMeta label="Genres" value={this.formatGenres(this.genres)} />
               <MovieMeta label="Runtime" value={`${movie.runtime} minutes`} />
-              <a className="external-link-icon imdb" target="_blank"
-                href={`https://www.imdb.com/title/${movie.imdb_id}`}
-              >
-                <FontAwesomeIcon icon={faImdb} />
-              </a>
+              <div className="external-links">
+                {movie.imdb_id && (
+                  <a className="external-link-icon imdb" target="_blank"
+                    href={`https://www.imdb.com/title/${movie.imdb_id}`}
+                  >
+                    <FontAwesomeIcon icon={faImdb} />
+                  </a>
+                )}
+                {movie.homepage && (
+                  <a className="external-link-icon has-text-dark-grey" target="_blank" href={movie.homepage}>
+                    <FontAwesomeIcon icon={faGlobe} />
+                  </a>
+                )}
+              </div>
             </div>
             
             <h4 className="title is-4">Synopsis:</h4>
