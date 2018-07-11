5dc26fc04182af6a8a8984eba63c4b37118e62e3
Philippe Luchansky <philuchansky@gmail.com>
2018-07-11 14:45:27 -0700

    stubbed removeFromQueue() action dispatcher

--- client/src/actions/queue.js
+++ client/src/actions/queue.js
@@ -1,5 +1,5 @@
 import httpClient from '../httpClient'
-import { ADD_TO_QUEUE } from '../types/queue'
+import { ADD_TO_QUEUE, REMOVE_FROM_QUEUE } from '../types/queue'
 
 const addToQueueLoading = () => ({ type: ADD_TO_QUEUE.LOADING })
 const addToQueueError = () => ({ type: ADD_TO_QUEUE.ERROR })
@@ -14,6 +14,24 @@ export function addToQueue(data) {
       })
       .catch((error) => {
         dispatch(addToQueueError(error.response.data.message))
+        return false
+      })
+  }
+}
+
+const removeFromQueueLoading = () => ({ type: REMOVE_FROM_QUEUE.LOADING })
+const removeFromQueueError = () => ({ type: REMOVE_FROM_QUEUE.ERROR })
+const removeFromQueueSuccess = (queueItem) => ({ type: REMOVE_FROM_QUEUE.SUCCESS, payload: queueItem })
+export function removeFromQueue(id) {
+  return (dispatch) => {
+    dispatch(removeFromQueueLoading())
+    return httpClient({ method: 'delete', url: `/queue/${id}` })
+      .then(({ data: { queueItem } }) => {
+        dispatch(removeFromQueueSuccess(queueItem))
+        return queueItem
+      })
+      .catch((error) => {
+        dispatch(removeFromQueueError(error.response.data.message))
         return false
       })
   }
