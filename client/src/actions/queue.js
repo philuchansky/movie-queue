454ef188ace5b9406ff830e1c9bcd153e644c1ad
Philippe Luchansky <philuchansky@gmail.com>
2018-07-11 14:34:34 -0700

    use status http codes to handle addToQueue errors

--- client/src/actions/queue.js
+++ client/src/actions/queue.js
@@ -2,13 +2,19 @@ import httpClient from '../httpClient'
 import { ADD_TO_QUEUE } from '../types/queue'
 
 const addToQueueLoading = () => ({ type: ADD_TO_QUEUE.LOADING })
-// const addToQueueError = () => ({ type: ADD_TO_QUEUE.ERROR })
+const addToQueueError = () => ({ type: ADD_TO_QUEUE.ERROR })
 const addToQueueSuccess = (queueItem) => ({ type: ADD_TO_QUEUE.SUCCESS, payload: queueItem })
 export function addToQueue(data) {
   return (dispatch) => {
     dispatch(addToQueueLoading())
-    httpClient({ method: 'post', url: '/queue', data }).then(({ data: { queueItem } }) => {
-      dispatch(addToQueueSuccess(queueItem))
-    })
+    return httpClient({ method: 'post', url: '/queue', data })
+      .then(({ data: { queueItem } }) => {
+        dispatch(addToQueueSuccess(queueItem))
+        return queueItem
+      })
+      .catch((error) => {
+        dispatch(addToQueueError(error.response.data.message))
+        return false
+      })
   }
 }
--- controllers/queue.js
+++ controllers/queue.js
@@ -10,15 +10,15 @@ module.exports = {
     QueueItem.create({ user: req.user._id, ...req.body })
       .then(queueItem => res.json({ success: true, message: 'queue item added', queueItem }))
       .catch(err => {
-        if(err.code === 11000) res.json({ success: false, message: 'queue item already exists' })
-        else res.json({ success: false, message: 'there was a problem' })
+        if(err.code === 11000) res.status(409).json({ success: false, message: 'queue item already exists' })
+        else res.status(403).json({ success: false, message: 'there was a problem' })
       })
   },
 
   destroy: (req, res) => {
     QueueItem.findByIdAndRemove(req.params.id).select('-user').exec()
       .then(queueItem => {
-        if(!queueItem) return res.json({ success: false, message: 'no queue item to remove' })
+        if(!queueItem) return res.status(409).json({ success: false, message: 'no queue item to remove' })
         res.json({ success: true, message: 'queue item removed', queueItem })
       })
   }
