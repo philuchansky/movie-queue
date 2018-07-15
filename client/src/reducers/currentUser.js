6d3218e22abe4352ae084a6d41de1f88da321070
Philippe Luchansky <philuchansky@gmail.com>
2018-07-15 16:09:13 -0700

    split currentUser reducer into loading, error, user

--- client/src/reducers/currentUser.js
+++ client/src/reducers/currentUser.js
@@ -1,13 +1,20 @@
 import { SIGN_UP, LOG_IN, LOG_OUT } from '../types/auth'
 
-export default (state = null, action) => {
+const initialState = { loading: false, user: null, error: null }
+
+export default (state = initialState, action) => {
   switch(action.type) {
     case LOG_IN.SUCCESS:
     case SIGN_UP.SUCCESS:
-      return action.payload
+      return { ...state, loading: false, user: action.payload }
+    case LOG_IN.LOADING:
+    case SIGN_UP.LOADING:
+      return { ...state, loading: true }
     case LOG_IN.ERROR:
+    case SIGN_UP.ERROR:
+      return { ...initialState, loading: false, error: action.payload }
     case LOG_OUT.SUCCESS:
-      return null
+      return initialState
     default: return state
   }
 }
