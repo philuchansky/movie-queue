1c880cc02fc001cb16014d3f506fa190b68e9d97
Philippe Luchansky <philuchansky@gmail.com>
2018-07-15 22:19:46 -0700

    render recommended movies in <MovieDetail />

--- client/src/components/MovieDetail/index.jsx
+++ client/src/components/MovieDetail/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
 import { getMovie } from '../../actions/movies'
 import { posterUrl, formattedDate } from '../../helpers'
@@ -7,6 +7,9 @@ import QueueButton from '../QueueButton'
 import Score from '../Score'
 import ExternalLinks from './ExternalLinks'
 import './MovieDetail.css'
+
+import _ from 'lodash'
+import MovieCard from '../MovieCard'
 
 class MovieDetail extends React.Component {
   componentDidMount() {
@@ -24,24 +27,38 @@ class MovieDetail extends React.Component {
       <div className="MovieDetail">
         {loading && <h1>Loading...</h1>}
         {movie && (
-          <div className="columns">
-            <div className="column is-one-third">
-              <img src={posterUrl(movie.poster_path, 'large')} alt={movie.title} />
-            </div>
-            <div className="column is-two-thirds">
-              <h1 className="title is-2">{movie.title}</h1>
-              <strong>Score</strong>: <Score percentage={movie.vote_average * 10} />
-              <div className="movie-metadata">
-                <MovieMeta label="Release Date" value={formattedDate(movie.release_date)} />
-                <MovieMeta label="Genres" value={this.formatGenres(movie.genres)} />
-                <MovieMeta label="Runtime" value={`${movie.runtime} minutes`} />
+          <Fragment>
+            <div className="columns">
+              <div className="column is-one-third">
+                <img src={posterUrl(movie.poster_path, 'large')} alt={movie.title} />
               </div>
-              {currentUser ? <QueueButton movie={movie} /> : null}
-              <h4 className="title is-4">Synopsis:</h4>
-              <p>{movie.overview}</p>
-              <ExternalLinks movie={movie} />
+              <div className="column is-two-thirds">
+                <h1 className="title is-2">{movie.title}</h1>
+                <strong>Score</strong>: <Score percentage={movie.vote_average * 10} />
+                <div className="movie-metadata">
+                  <MovieMeta label="Release Date" value={formattedDate(movie.release_date)} />
+                  <MovieMeta label="Genres" value={this.formatGenres(movie.genres)} />
+                  <MovieMeta label="Runtime" value={`${movie.runtime} minutes`} />
+                </div>
+                {currentUser ? <QueueButton movie={movie} /> : null}
+                <h4 className="title is-4">Synopsis:</h4>
+                <p>{movie.overview}</p>
+                <ExternalLinks movie={movie} />
+              </div>
             </div>
-          </div>
+            <h2 className="title">You Might Also Like</h2>
+            <div>
+              {_.chunk(movie.recommendations.slice(0, 10), 5).map((row, rowIdx) => (
+                <div key={rowIdx} className="columns">
+                  {row.map((movie, colIdx) => (
+                    <div key={colIdx} className="column">
+                      <MovieCard movie={movie} />
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </Fragment>
         )}
       </div>
     )
