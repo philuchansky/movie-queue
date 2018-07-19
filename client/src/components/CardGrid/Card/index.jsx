c88190fcef7bc336b30cbc22169b234f5291d8f5
Philippe Luchansky <philuchansky@gmail.com>
2018-07-18 19:01:43 -0700

    missingAvatar image, refactored <Card />

--- client/src/components/CardGrid/Card/index.jsx
+++ client/src/components/CardGrid/Card/index.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { tmdbImgUrl } from '../../../helpers'
+import missingAvatar from './images/missingAvatar.png'
 import './Card.css'
 
 const Card = (props) => {
   const { data, type, showLabel } = props
   const label = data.title || data.name
-  const imgPath = data.poster_path || data.profile_path
+  const imgPath = {
+    "movies": data.poster_path ? tmdbImgUrl(data.poster_path) : '',
+    "people": data.profile_path ? tmdbImgUrl(data.profile_path) : missingAvatar
+  }[type]
+  
   return (
     <div className="Card card">
       <div className="card-image">
         <figure className="image">
           <Link to={`/${type}/${data.id}`}>
-            <img src={tmdbImgUrl(imgPath, 'medium')} alt={label} />
+            <img src={imgPath} alt={label} />
             {showLabel ? label : null}
           </Link>
         </figure>
