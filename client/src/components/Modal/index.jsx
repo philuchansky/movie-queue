c8a79a3aa579f53636bfd73dfbf45edcc8bf71ae
Philippe Luchansky <philuchansky@gmail.com>
2018-07-19 14:20:07 -0700

    connect <Modal /> to modal reducer, use active boolean for "is-active" class

--- client/src/components/Modal/index.jsx
+++ client/src/components/Modal/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 const Modal = (props) => {
+  const { modal: { active } } = props
   return (
     <div className="Modal">
-      <div className="modal">
+      <div className={`modal ${active ? 'is-active' : ''}`}>
         <div className="modal-background"></div>
         <div className="modal-content">
           <p className="image is-4by3">
@@ -16,4 +18,5 @@ const Modal = (props) => {
   )
 }
 
-export default Modal
+const mapStateToProps = ({ modal }) => ({ modal })
+export default connect(mapStateToProps)(Modal)
