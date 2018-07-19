a8bfa3d3d4d3b22af085396c3bc640dc21783064
Philippe Luchansky <philuchansky@gmail.com>
2018-07-19 14:11:03 -0700

    modal html example structure from bulma

--- client/src/components/Modal/index.jsx
+++ client/src/components/Modal/index.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Modal = (props) => {
   return (
     <div className="Modal">
-      <h1>Modal goes here...</h1>
+      <div className="modal is-active">
+        <div className="modal-background"></div>
+        <div className="modal-content">
+          <p className="image is-4by3">
+            <img src="https://bulma.io/images/placeholders/1280x960.png" alt="" />
+          </p>
+        </div>
+        <button className="modal-close is-large" aria-label="close"></button>
+      </div>
     </div>
   )
 }
