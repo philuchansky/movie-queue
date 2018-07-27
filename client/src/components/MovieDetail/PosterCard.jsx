ec86842c5b14c3078ab57816948a40ffb0df401f
Philippe Luchansky <philuchansky@gmail.com>
2018-07-27 09:46:44 -0700

    use <Card /> in <PosterCard />

--- client/src/components/MovieDetail/PosterCard.jsx
+++ client/src/components/MovieDetail/PosterCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { openModalImage } from '../../actions/modal'
 import { tmdbImgUrl } from '../../helpers'
+import Card from '../Card'
 
 const handlePosterCardClick = (actionDispatcher, movie) => {
   actionDispatcher(movie.title, movie.poster_path)
@@ -10,17 +11,8 @@ const handlePosterCardClick = (actionDispatcher, movie) => {
 const PosterCard = (props) => {
   const { movie, openModalImage } = props
   return (
-    <div className="poster Card card"
-      onClick={handlePosterCardClick.bind(null, openModalImage, movie)}
-    >
-      <div className="card-image">
-        <figure className="image">
-          <img
-            src={tmdbImgUrl(movie.poster_path, 'large')}
-            alt={movie.title}
-          />
-        </figure>
-      </div>
+    <div onClick={handlePosterCardClick.bind(null, openModalImage, movie)}>
+      <Card imgSrc={tmdbImgUrl(movie.poster_path)} />
     </div>
   )
 }
