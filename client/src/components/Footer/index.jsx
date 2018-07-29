7e53e598c9220fdeb81e5bb21a632b2997ae56a7
Philippe Luchansky <philuchansky@gmail.com>
2018-07-29 10:04:21 -0700

    <Footer /> styles and copy

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import poweredByTmdb from './images/poweredByTmdb.svg'
 import './Footer.css'
 
+const year = (new Date()).getFullYear()
+
 const Footer = (props) => {
   return (
-    <nav className="Footer">
-      <div className="navbar is-warning">
-        <div className="container has-text-centered">
-          <a href="https://www.themoviedb.org/" rel="noopener noreferrer" target="_blank">
-            <img className="poweredByTmdb" src={poweredByTmdb} alt="Powered By The Movie DB" />
-          </a>
-        </div>
+    <footer className="Footer">
+      <div className="container has-text-centered is-size-7">
+        <span className="copyright">© {year}. Film data from <a href="https://www.themoviedb.org" target="_blank">TMDb</a>.</span>
+        <a href="https://www.themoviedb.org" rel="noopener noreferrer" target="_blank">
+          <img className="poweredByTmdb" src={poweredByTmdb} alt="Powered By The Movie DB" />
+        </a>
       </div>
-    </nav>
+    </footer>
   )
 }
 
